bf2e6da8cc604db60ee4356386b8310f49300462
Sébastien Pondichy <sebastien.pondichy@gmail.com>
2024-11-28 17:13:12 +0100

    feat(pipewire): enable user services

--- pipewire.sh
+++ pipewire.sh
@@ -1,2 +1,6 @@
 #!/usr/bin/env bash
+echo "Installing pipewire-audio meta package from bookworm-backports"
 sudo apt install -t bookworm-backports pipewire-audio
+
+echo "Enable pipewire related user services"
+systemctl --user --now enable pipewire.service pipewire-pulse.service wireplumber.service
