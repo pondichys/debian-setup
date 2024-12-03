9c3e843f619cf75ac7306e95db15bbaa4b41f6cd
Sébastien Pondichy <sebastien.pondichy@gmail.com>
2024-12-03 07:37:15 +0100

    chore(wezterm): replace nala by apt

--- wezterm.sh
+++ wezterm.sh
@@ -4,7 +4,7 @@ curl -fsSL https://apt.fury.io/wez/gpg.key | sudo gpg --yes --dearmor -o /usr/sh
 echo 'deb [signed-by=/usr/share/keyrings/wezterm-fury.gpg] https://apt.fury.io/wez/ * *' | sudo tee /etc/apt/sources.list.d/wezterm.list
 
 echo " Updating package database and installing wezterm"
-sudo nala update && sudo nala install wezterm
+sudo apt update && sudo apt install wezterm
 
 if [ "$?" -eq 0 ]; then
 	echo "Wezterm successfully installed"
