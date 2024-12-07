6bf54bc33466bc668ad5b43174f2990aacd8d8ea
Sébastien Pondichy <sebastien.pondichy@gmail.com>
2024-12-07 09:54:55 +0100

    feat(fonts): WIP - initial commit

---
+++ nerdfonts.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+# Get latest release from Github
+# Download a list in a for loop based on font's name
+# Extract the fonts from the compressed archive
+# Move the resulting directory to $HOME/.local/share/fonts
