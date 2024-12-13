ee79d0ec1d467d6d7b4af688c230bf946b922d94
Sébastien Pondichy <sebastien.pondichy@gmail.com>
2024-12-13 12:11:21 +0100

    fix(gnome): only install extension-manager from debian repo
    All other gnome extensions are installed from extension-manager as user
    packages

--- gnome.sh
+++ gnome.sh
@@ -2,11 +2,11 @@
 echo "Installing gnome core package and required dependencies ..."
 sudo apt install -y gnome-core gnome-tweaks
 
-echo "Installing some gnome extensions"
+echo "Installing gnome extension manager"
 sudo apt install -y \
-	gnome-shell-extension-appindicator \
-	gnome-shell-extension-caffeine \
-	gnome-shell-extension-dashtodock \
+#	gnome-shell-extension-appindicator \
+#	gnome-shell-extension-caffeine \
+#	gnome-shell-extension-dashtodock \
 	gnome-shell-extension-manager
 
 echo "Its now time to reboot to activate the changes!"
