a648dbd38038294974e81e9f1b46ca09490d37a2
Sébastien Pondichy <sebastien.pondichy@gmail.com>
2024-12-20 09:54:10 +0100

    chore(brave): use extrepo to manage brave repository

--- brave.sh
+++ brave.sh
@@ -1,12 +1,7 @@
 #!/usr/bin/env bash
-echo "Installing Brave browser"
-
-sudo apt install curl
-
-sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-
-echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-
+echo "Enabling brave repository"
+sudo extrepo enable brave_release
 sudo apt update
 
+echo "Installing Brave browser"
 sudo apt install brave-browser
