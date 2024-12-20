b5758a0aa3bbebac1142a10936ce352b5dc5a74b
Sébastien Pondichy <sebastien.pondichy@gmail.com>
2024-12-20 09:53:00 +0100

    chore: cleanup base.sh and remove lazygit.sh

--- base.sh
+++ base.sh
@@ -1,25 +1,14 @@
 #!/usr/bin/env bash
 
-function handle_error() {
-    echo "Error: $1"
-    exit 1
-}
-
-# Enable debian test?
-# Following packages are not present or too old
-# old
-# -fzf 
-# -zoxide
-# absent:
-# - chezmoi
-# - fastfetch
-# - lazygit
-# - starship
-# - yazi
-# - zellij
+echo "Installing base utilities"
 sudo apt install bat curl fd-find fzf git jq ripgrep tmux vim wget
 
-# sudo apt install fish
+"Install fish shell"
+sudo apt install fish
+
+echo "Installing curated list of external repositories"
+sudo apt install extrepo
+echo "You can use extrepo enable ... to enable external repositories like Librewolf, Brave, VS Code, etc ..."
 
 echo "Creating $HOME/.local directories"
 if [ ! -d $HOME/.local/bin ]; then
@@ -29,28 +18,5 @@ fi
 if [ ! -d $HOME/.local/share/fonts ]; then
 	mkdir -pv $HOME/.local/share/{fonts,icons,themes}
 fi
-
-if ! command -v zoxide &> /dev/null
-then
-    echo "Installing zoxide from github"
-    curl -sSfl https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | sh
-fi
-
-if ! command -v fastfetch &> /dev/null
-then
-    echo "Installing fastfetch from github"
-    ff_url = $(curl -s https://api.github.com/repos/fastfetch-cli/fastfetch/releases/latest | jq -r '.assets[] | select(.name | test(".*linux-amd64.deb$")) | .browser_download_url')
-    wget $ff_url -o fastfetch-linux-amd64.deb
-    sudo apt install ./fastfetch-linux-amd64.deb
-fi
-
-if ! command -v chezmoi &> /dev/null
-then
-    echo "Installing chezmoi dotfiles manager"
-    cd $HOME && sh -c "$(curl -fsLS get.chezmoi.io/lb)"
-fi
-
-# Install distrobox
-# curl -s https://raw.githubusercontent.com/89luca89/distrobox/main/install | sh -s -- --prefix ~/.local
 
 echo "All base utilities are installed"
