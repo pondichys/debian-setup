555b224606af504e1cbd78b3b41ea411b346e259
Sébastien Pondichy <sebastien.pondichy@gmail.com>
2024-12-20 09:57:05 +0100

    chore(virtualbox): use extrepo to manage virtualbox repository

--- virtualbox.sh
+++ virtualbox.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
+echo "Enabling VirtualBox repository"
+sudo extrepo enable virtualbox
 
-echo "deb [arch=amd64 signed-by=/usr/share/keyrings/oracle-virtualbox-2016.gpg] https://download.virtualbox.org/virtualbox/debian bookworm contrib" | sudo tee /etc/apt/sources.list.d/virtualbox.list
-
-wget -O- https://www.virtualbox.org/download/oracle_vbox_2016.asc | sudo gpg --yes --output /usr/share/keyrings/oracle-virtualbox-2016.gpg --dearmor
-
-sudo apt-get update
-sudo apt-get install virtualbox-7.1
+echo "Installing VirtualBox"
+sudo apt update
+sudo apt install virtualbox-7.1
