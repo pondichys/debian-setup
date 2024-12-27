fcf85eccc4a27dd2ece1d9feca843b6971f038f7
Sébastien Pondichy <sebastien.pondichy@gmail.com>
2024-12-27 08:57:00 +0100

    chore(base): replace apt by apt-get
    apt-get uses more stable outputs and is preferred for scripting as per
    Debian official doc

--- base.sh
+++ base.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
 echo "Installing base utilities"
-sudo apt install bat curl fd-find fzf git jq ripgrep tmux vim wget
+# wl-clipboard and xsel are needed for tmux clipboard integration under wayland / x11
+sudo apt-get install bat curl fd-find git jq ripgrep tmux vim wget wl-clipboard xsel
 
 "Install fish shell"
-sudo apt install fish
+sudo apt-get install fish
 
 echo "Installing curated list of external repositories"
-sudo apt install extrepo
+sudo apt-get install extrepo
 echo "You can use extrepo enable ... to enable external repositories like Librewolf, Brave, VS Code, etc ..."
 
 echo "Creating $HOME/.local directories"
