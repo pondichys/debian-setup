cddba425fd2c0a257e5074bfb60e341b2110a9fe
Sébastien Pondichy <sebastien.pondichy@gmail.com>
2025-10-13 07:51:39 +0200

    feat(gnome): add ptyxis and network-manager-applet
    - comment out gnome extensions installation commands
    - remove zutty terminal that comes with gnome-core
    - set empty /etc/networkk/interfaces so that Gnome can manage network

--- gnome.sh
+++ gnome.sh
@@ -1,13 +1,19 @@
 #!/usr/bin/env bash
 echo "Installing gnome core package and required dependencies ..."
-sudo apt install -y gnome-core gnome-tweaks
+sudo apt-get install -y gnome-core gnome-tweaks ptyxis network-manager-applet
 
-echo "Installing gnome extension manager"
-sudo apt install -y \
+#echo "Installing gnome extension manager"
+#sudo apt install -y \
 #	gnome-shell-extension-appindicator \
 #	gnome-shell-extension-caffeine \
 #	gnome-shell-extension-dashtodock \
-	gnome-shell-extension-manager
+#	gnome-shell-extension-manager
+echo "Removing zutty terminal"
+sudo apt-get remove zutty
+
+echo "Removing /etc/network/interfaces so that Gnome can manage network."
+sudo mv -i /etc/network/interfaces /etc/network/interfaces.bak
+sudo touch /etc/network/interfaces
 
 echo "Its now time to reboot to activate the changes!"
 echo "Run systemctl reboot to restart your system."
