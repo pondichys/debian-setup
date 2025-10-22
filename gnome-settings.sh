05feb67f2eb2873104ef30fb2d592b13d5aaf019
Sébastien Pondichy <sebastien.pondichy@gmail.com>
2025-10-22 09:39:16 +0200

    feat(gnome-settings): enable minimize and maximize window buttons

--- gnome-settings.sh
+++ gnome-settings.sh
@@ -2,3 +2,7 @@
 
 echo "Enable numlock on Gnome startup"
 gsettings set org.gnome.desktop.peripherals.keyboard numlock-state true
+
+echo "Enable minimize and maximize buttons"
+gsettings set org.gnome.desktop.wm.preferences button-layout ":minimize,maximize,close"
+
